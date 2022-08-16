a0cba04d2b2e1a19f97bcf1b786be303e95fb5fe
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-08-16 12:38:05 -0500

    first render <3!

--- lesson3/script.js
+++ lesson3/script.js
@@ -4,4 +4,32 @@ const scene = new THREE.Scene();
 // Cubo rojo
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "#e3bfd4" });
+const mesh = new THREE.Mesh(geometry, material);
 
+// Agregar cubo al scene
+scene.add(mesh);
+
+// Tamaños para la camara
+const sizes = {
+  widht: 800,
+  height: 600,
+};
+
+// Camara
+const camera = new THREE.PerspectiveCamera(75, sizes.widht / sizes.height);
+scene.add(camera);
+
+// Mover la camara 
+camera.position.z = 3;
+
+// Canvas con querySelector
+const canvas = document.querySelector("canvas.webgl");
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.widht, sizes.height);
+
+// Render
+renderer.render(scene, camera);
