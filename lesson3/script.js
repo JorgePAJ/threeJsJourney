4aafa6582301220ae7cc03b836582372098697b3
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-08-16 12:09:06 -0500

    Added typings.js {Intellisense support}

--- lesson3/script.js
+++ lesson3/script.js
@@ -1,1 +1,7 @@
-console.log("Hello ThreeJS");
+//Scene
+const scene = new THREE.Scene();
+
+// Cubo rojo
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "#e3bfd4" });
+
