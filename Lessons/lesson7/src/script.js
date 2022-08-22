1efa499e0b63f82b7eac75910495085c46d3d9b0
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-08-22 14:02:32 -0500

    lesson concluded

--- Lessons/lesson7/src/script.js
+++ Lessons/lesson7/src/script.js
@@ -1,5 +1,7 @@
+// Imports
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 // Cursor
 const cursor = {
@@ -58,6 +60,12 @@ camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+// controls.target.y = 1;
+// controls.update()
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -73,11 +81,14 @@ const tick = () => {
   // Update objects
   //   mesh.rotation.y = elapsedTime;
 
-  // Update camera
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  camera.position.y = cursor.y * 5;
-  camera.lookAt(mesh.position);
+  // Update camera {Comentado por que los controles los manejara orbitControls}
+  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.y = cursor.y * 5;
+  //   camera.lookAt(mesh.position);
+
+  // Update controls
+  controls.update()
 
   // Render
   renderer.render(scene, camera);
