8b317a1907e9a94d0a3ce73f78fa9592fa395302
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-08-23 12:15:45 -0500

    snippet follow mouse

--- Lessons/lesson7/src/script.js
+++ Lessons/lesson7/src/script.js
@@ -61,8 +61,8 @@ camera.lookAt(mesh.position);
 scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
 // controls.target.y = 1;
 // controls.update()
 
@@ -82,13 +82,13 @@ const tick = () => {
   //   mesh.rotation.y = elapsedTime;
 
   // Update camera {Comentado por que los controles los manejara orbitControls}
-  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  //   camera.position.y = cursor.y * 5;
-  //   camera.lookAt(mesh.position);
+  camera.position.x = Math.sin((cursor.x * Math.PI) / 2) * 3;
+  camera.position.z = Math.cos((cursor.x * Math.PI) / 2) * 3;
+  camera.position.y = Math.sin((cursor.y * Math.PI) / 2) * 3
+  camera.lookAt(mesh.position);
 
   // Update controls
-  controls.update()
+  // controls.update();
 
   // Render
   renderer.render(scene, camera);
