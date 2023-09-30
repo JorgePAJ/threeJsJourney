8e518e28d19ce897f1ef864a787d0e20043cb4cd
Jorge Plasencia <jorgeplasenciaaj@gmail.com>
2023-09-30 10:48:02 -0600

    lesson concluded

--- Lessons/lesson48/src/Experience.jsx
+++ Lessons/lesson48/src/Experience.jsx
@@ -1,18 +1,77 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import {
+  OrbitControls,
+  Text3D,
+  Center,
+  useMatcapTexture,
+} from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Perf } from "r3f-perf";
+import { useState, useEffect, useRef } from "react";
+import * as THREE from "three";
 
-export default function Experience()
-{
-    return <>
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32);
+const material = new THREE.MeshMatcapMaterial();
 
-        <Perf position="top-left" />
+export default function Experience() {
+  const donuts = useRef([]);
 
-        <OrbitControls makeDefault />
+  useEffect(() => {
+    matcapTexture.colorSpace = THREE.SRGBColorSpace;
+    matcapTexture.needsUpdate = true;
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+    material.matcap = matcapTexture;
+    material.needsUpdate = true;
+  }, []);
 
+  useFrame((state, delta) => {
+    for (const donut of donuts.current) {
+      donut.rotation.y += delta * 0.2;
+    }
+  });
+
+  const [matcapTexture] = useMatcapTexture("7B5254_E9DCC7_B19986_C8AC91", 256);
+
+  //   const [torusGeometry, setTorusGeometry] = useState();
+  //   const [material, setMaterial] = useState();
+
+  return (
+    <>
+      <Perf position="top-left" />
+      <OrbitControls makeDefault />
+      {/* <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
+      <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} />; */}
+      <Center>
+        <Text3D
+          font="./fonts/Geomanist_Medium.json"
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+          material={material}
+        >
+          Hola plebes!
+        </Text3D>
+      </Center>
+      {[...Array(100)].map((value, index) => (
+        <mesh
+          ref={(element) => {
+            donuts.current[index] = element;
+          }}
+          key={index}
+          geometry={torusGeometry}
+          material={material}
+          position={[
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10,
+          ]}
+          scale={0.2 + Math.random() * 0.2}
+          rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+        />
+      ))}
     </>
+  );
 }
